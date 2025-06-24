78d5ee4385d3322a8438bcdb40baf9dfaf499e34
Nehil Sahu <nehilsahu281@gmail.com>
2025-06-24 14:44:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -50,4 +50,8 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server running on ${port}`));
+
+
 module.exports = app;
